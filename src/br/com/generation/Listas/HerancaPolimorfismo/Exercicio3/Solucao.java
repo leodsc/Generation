a90a9d82887f42f4c0fe63dd0d2bac6708e75e21
Leonardo <leodscarvalho@gmail.com>
2022-02-02 21:06:33 -0300

    Adicionando exercicio 3 de polimorfismo

--- src/br/com/generation/Listas/HerancaPolimorfismo/Exercicio3/Solucao.java
+++ src/br/com/generation/Listas/HerancaPolimorfismo/Exercicio3/Solucao.java
@@ -2,6 +2,7 @@ package br.com.generation.Listas.HerancaPolimorfismo.Exercicio3;
 
 public class Solucao {
 	public static void main(String[] args) {
-		
+		Menu menu = new Menu();
+		menu.apresentacao();
 	}
 }
