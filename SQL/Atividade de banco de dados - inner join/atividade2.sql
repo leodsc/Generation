dae421ed97edbe0319653ce85d2b1bef5fd0495c
Leonardo <leodscarvalho@gmail.com>
2022-02-15 21:08:30 -0300

    update: atividade 2 foi arrumada

--- SQL/Atividade de banco de dados - inner join/atividade2.sql
+++ SQL/Atividade de banco de dados - inner join/atividade2.sql
@@ -3,37 +3,36 @@ create database db_pizzaria_legal;
 use db_pizzaria_legal;
 create table tb_categorias(
 	id bigint auto_increment,
-    pizzaiolo varchar(16) not null,
-    especialidade varchar(8) not null,
-    horario_trabalho varchar(32) not null,
+    tipo varchar(16) not null,
+    tem_borda boolean not null,
+    borda_recheada varchar(32),
     
     primary key(id)
 );
 
 /* popule essa tabela com ate 5 dados */
+insert into tb_categorias(tipo, tem_borda, borda_recheada)
+	values("vegana", false, null);
 
-insert into tb_categorias(pizzaiolo, especialidade, horario_trabalho)
-	 values("Ronaldo", "doce", "10:00:00-18:00:00");
-     
-insert into tb_categorias(pizzaiolo, especialidade, horario_trabalho)
-	 values("Nayara", "salgada", "14:00:00-22:00:00");
-     
-insert into tb_categorias(pizzaiolo, especialidade, horario_trabalho)
-	 values("Kellen", "doce", "14:00:00-22:00:00");
-     
-insert into tb_categorias(pizzaiolo, especialidade, horario_trabalho)
-	 values("Roberto", "salgada", "10:00:00-18:00:00");
-     
-insert into tb_categorias(pizzaiolo, especialidade, horario_trabalho)
-	 values("Bernardo", "salgada", "10:00:00-18:00:00");
+insert into tb_categorias(tipo, tem_borda, borda_recheada)
+	values("normal", true, "cheddar");
+    
+insert into tb_categorias(tipo, tem_borda, borda_recheada)
+	values("vegana", true, "queijo vegano");
+    
+insert into tb_categorias(tipo, tem_borda, borda_recheada)
+	values("normal", false, null);
+    
+insert into tb_categorias(tipo, tem_borda, borda_recheada)
+	values("normal", true, "queijo");
 
 create table tb_pizza(
 	id bigint auto_increment,
     categoria_id bigint,
     sabor varchar(32) not null,
     preco double not null,
-    borda_recheada boolean NOT NULL,
-    borda_sabor varchar(32),
+    entrega time not null,
+    previsao_entrega time not null,
     tamanho varchar(16),
     
     primary key (id),
@@ -42,29 +41,29 @@ create table tb_pizza(
 
 /* popule essa tabela com ate 8 dados */
 
-insert into tb_pizza(sabor, categoria_id, preco, borda_recheada, borda_sabor, tamanho)
-	values("Calabresa", 2, 32.50, true, "cheddar", "grande");
+insert into tb_pizza(sabor, categoria_id, preco, entrega, previsao_entrega, tamanho)
+	values("Calabresa", 2, 32.50, "20:40:00", "20:42:00", "grande");
     
-insert into tb_pizza(sabor, categoria_id, preco, borda_recheada, borda_sabor, tamanho)
-	values("4 Queijos", 5, 20.00, false, null, "medio");
+insert into tb_pizza(sabor, categoria_id, preco, entrega, previsao_entrega, tamanho)
+	values("4 Queijos", 5, 20.00, "19:48:00", "19:40:00", "medio");
         
-insert into tb_pizza(sabor, categoria_id, preco, borda_recheada, borda_sabor, tamanho)
-	values("Frango c Catupiry", 2, 35.00, true, "queijo", "grande");
+insert into tb_pizza(sabor, categoria_id, preco, entrega, previsao_entrega, tamanho)
+	values("Frango c Catupiry", 2, 35.00, "22:01:35", "21:58:00", "grande");
         
-insert into tb_pizza(sabor, categoria_id, preco, borda_recheada, borda_sabor, tamanho)
-	values("Nutella", 3, 65.00, false, null, "grande");
+insert into tb_pizza(sabor, categoria_id, preco, entrega, previsao_entrega, tamanho)
+	values("Nutella", 4, 65.00, "17:32:05", "17:30:00", "grande");
         
-insert into tb_pizza(sabor, categoria_id, preco, borda_recheada, borda_sabor, tamanho)
-	values("Frango c Catupiry", 4, 27.50, false, null, "grande");
+insert into tb_pizza(sabor, categoria_id, preco, entrega, previsao_entrega, tamanho)
+	values("Frango c Catupiry", 4, 27.50, "18:39:42", "19:00:00", "grande");
         
-insert into tb_pizza(sabor, categoria_id, preco, borda_recheada, borda_sabor, tamanho)
-	values("Calabresa", 2, 14.00, true, "queijo", "pequena");
+insert into tb_pizza(sabor, categoria_id, preco, entrega, previsao_entrega, tamanho)
+	values("Calabresa", 2, 14.00, "12:05:07", "11:50:00", "pequena");
         
-insert into tb_pizza(sabor, categoria_id, preco, borda_recheada, borda_sabor, tamanho)
-	values("M&Ms", 1, 75.00, false, null, "medio");
+insert into tb_pizza(sabor, categoria_id, preco, entrega, previsao_entrega, tamanho)
+	values("M&Ms", 1, 75.00, "13:10:00", "13:00:00", "medio");
     
-insert into tb_pizza(sabor, categoria_id, preco, borda_recheada, borda_sabor, tamanho)
-	values("Calabresa", 4, 24.00, true, "cheddar", "medio");
+insert into tb_pizza(sabor, categoria_id, preco, entrega, previsao_entrega, tamanho)
+	values("Calabresa", 4, 24.00, "23:07:05", "23:10:00", "medio");
 
 /* faça um select que retorne os produtos com o valor amior do que 45 reais. */
 
