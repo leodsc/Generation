27668c7cb9ee883f421ac4c4dda655b84779b112
Leonardo <leodscarvalho@gmail.com>
2022-03-10 10:16:17 -0300

    Add: testes unitarios

--- Blog Pessoal/src/main/java/com/leodsc/blog/BlogApplication.java
+++ Blog Pessoal/src/main/java/com/leodsc/blog/BlogApplication.java
@@ -2,6 +2,8 @@ package com.leodsc.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BlogApplication {
@@ -9,4 +11,6 @@ public class BlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
+
+
 }
